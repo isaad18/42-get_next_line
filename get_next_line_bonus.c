eec9c27740c1be5da29c57effb2c7928df4be496
Imad Saad <isaad@MAD.local>
2022-02-06 16:36:06 +0400

    o

---
+++ get_next_line_bonus.c
@@ -0,0 +1,103 @@
+#include"get_next_line_bonus.h"
+
+char	*ft_line(char *str)
+{
+	int		i;
+	char	*s;
+
+	i = 0;
+	if (!str)
+		return (NULL);
+	while (str[i] != '\0' && str[i] != '\n')
+		i++;
+	if (str[i] == '\n')
+		s = (char *)malloc(i + 2);
+	else
+		s = (char *)malloc(i + 1);
+	if (!s)
+		return (NULL);
+	i = 0;
+	while (str[i] != '\0' && str[i] != '\n')
+	{
+		s[i] = str[i];
+		i++;
+	}
+	if (str[i] == '\n')
+	{
+		s[i] = str[i];
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
+
+char	*ft_next(char *str)
+{
+	int		i;
+	char	*s;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (!str[i])
+	{
+		free (str);
+		return (NULL);
+	}
+	s = (char *)malloc(ft_strlen(str) - i + 1);
+	if (!s)
+		return (NULL);
+	i++;
+	while (str[i])
+		s[j++] = str[i++];
+	s[j] = '\0';
+	free (str);
+	return (s);
+}
+
+char	*ft_read(int fd, char *new)
+{
+	int		i;
+	char	*s;
+
+	i = 1;
+	s = (char *)malloc(BUFFER_SIZE + 1);
+	if (!s)
+		return (NULL);
+	while (!ft_strchr(new, '\n') && i != 0)
+	{
+		i = read (fd, s, BUFFER_SIZE);
+		if (i == -1)
+		{
+			free (s);
+			return (NULL);
+		}
+		s[i] = '\0';
+		new = ft_strjoin(new, s);
+	}
+	if (new[0] == '\0')
+	{
+		free (new);
+		free (s);
+		return (NULL);
+	}
+	free (s);
+	return (new);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*str[OPEN_MAX];
+	char		*final;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || fd > OPEN_MAX)
+		return (NULL);
+	str[fd] = ft_read(fd, str[fd]);
+	if (!str[fd])
+		return (NULL);
+	final = ft_line(str[fd]);
+	str[fd] = ft_next(str[fd]);
+	return (final);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,22 @@
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include<unistd.h>
+# include<stdlib.h>
+# include<stdio.h>
+# include<limits.h>
+# include<sys/select.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 999999999
+# endif
+
+size_t	ft_strlen(const char *s);
+
+char	*ft_strjoin(char *s1, char *s2);
+
+char	*ft_strchr(const char *s, int c);
+
+char	*get_next_line(int fd);
+
+#endif
